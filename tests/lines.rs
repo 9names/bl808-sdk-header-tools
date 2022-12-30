a429f63cce9ecaf2c2668f04c1cbafa03829a73d
9names <60134748+9names@users.noreply.github.com>
2022-12-30 16:55:54 +1100

    Add basic parsing infrastructure

---
+++ tests/lines.rs
@@ -0,0 +1,169 @@
+#[cfg(test)]
+mod tests {
+    use bl808_sdk_header_tools::*;
+    #[test]
+    fn parse_register_comment() {
+        let teststring = String::from("    /* 0x0 : soc_info0 */");
+        let (state, parseresult) = parse(ParseState::BlockName, teststring, 0);
+        assert_eq!(state, ParseState::BlockAddr);
+        assert!(parseresult.is_some());
+        if let Some(x) = parseresult {
+            match x {
+                ParseResult::Match(_) => panic!("Should contain capture"),
+                ParseResult::Capture(r) => {
+                    let mut r_iter = r.iter();
+                    assert_eq!(r_iter.next().unwrap(), "0x0");
+                    assert_eq!(r_iter.next().unwrap(), "soc_info0");
+                }
+            }
+        };
+    }
+
+    #[test]
+    fn parse_register_field() {
+        let teststring =
+            String::from("uint32_t reserved_0_26 : 27; /* [26: 0],       rsvd,        0x0 */");
+        let (state, parseresult) = parse(ParseState::StructStr, teststring, 0);
+        assert_eq!(state, ParseState::StructStr);
+        assert!(parseresult.is_some());
+        if let Some(x) = parseresult {
+            match x {
+                ParseResult::Match(_) => panic!("Should contain capture"),
+                ParseResult::Capture(r) => {
+                    let mut r_iter = r.iter();
+                    // assert_eq!(r_iter.next().unwrap(), "uint32_t");
+                    assert_eq!(r_iter.next().unwrap(), "reserved_0_26");
+                    assert_eq!(r_iter.next().unwrap(), "27");
+                    assert_eq!(r_iter.next().unwrap(), "26: 0");
+                    assert_eq!(r_iter.next().unwrap(), "rsvd");
+                    assert_eq!(r_iter.next().unwrap(), "0x0");
+                }
+            }
+        };
+    }
+
+    #[test]
+    fn parse_register_field2() {
+        let teststring =
+            String::from("uint32_t chip_rdy      : 1;  /* [   27],          r,        0x0 */");
+        let (state, parseresult) = parse(ParseState::StructStr, teststring, 0);
+        assert_eq!(state, ParseState::StructStr);
+        assert!(parseresult.is_some());
+        if let Some(x) = parseresult {
+            match x {
+                ParseResult::Match(_) => panic!("Should contain capture"),
+                ParseResult::Capture(r) => {
+                    let mut r_iter = r.iter();
+                    // assert_eq!(r_iter.next().unwrap(), "uint32_t");
+                    assert_eq!(r_iter.next().unwrap(), "chip_rdy");
+                    assert_eq!(r_iter.next().unwrap(), "1");
+                    assert_eq!(r_iter.next().unwrap(), "   27");
+                    assert_eq!(r_iter.next().unwrap(), "r");
+                    assert_eq!(r_iter.next().unwrap(), "0x0");
+                }
+            }
+        };
+    }
+
+    #[test]
+    fn parse_union() {
+        let teststring = String::from("    union {");
+        let (state, parseresult) = parse(ParseState::BlockAddr, teststring, 0);
+        assert_eq!(state, ParseState::UnionStr);
+        assert!(parseresult.is_some());
+        if let Some(x) = parseresult {
+            match x {
+                ParseResult::Match(r) => {
+                    let mut r_iter = r.iter();
+                    assert_eq!(r_iter.next().unwrap(), "    union {");
+                }
+                ParseResult::Capture(r) => panic!("Should contain match"),
+            }
+        };
+    }
+
+    #[test]
+    fn parse_struct() {
+        let teststring = String::from("        struct {");
+        let (state, parseresult) = parse(ParseState::UnionStr, teststring, 0);
+        assert_eq!(state, ParseState::StructStr);
+        assert!(parseresult.is_some());
+        if let Some(x) = parseresult {
+            match x {
+                ParseResult::Match(r) => {
+                    let mut r_iter = r.iter();
+                    assert_eq!(r_iter.next().unwrap(), "        struct {");
+                }
+                ParseResult::Capture(r) => panic!("Should contain match"),
+            }
+        };
+    }
+
+    #[test]
+    fn parse_peri_struct() {
+        let teststring = String::from("struct glb_reg {");
+        let (state, parseresult) = parse(ParseState::NoMatch, teststring, 0);
+        assert_eq!(state, ParseState::BlockName);
+        assert!(parseresult.is_some());
+        if let Some(x) = parseresult {
+            match x {
+                ParseResult::Match(_) => panic!("Should contain capture"),
+                ParseResult::Capture(r) => {
+                    let mut r_iter = r.iter();
+                    assert_eq!(r_iter.next().unwrap(), "glb_reg");
+                }
+            }
+        };
+    }
+
+    #[test]
+    fn parse_bf() {
+        let teststring = String::from("        } BF;");
+        let (state, parseresult) = parse(ParseState::Field, teststring, 0);
+        assert_eq!(state, ParseState::Size);
+        assert!(parseresult.is_some());
+        if let Some(x) = parseresult {
+            match x {
+                ParseResult::Match(r) => {
+                    let mut r_iter = r.iter();
+                    assert_eq!(r_iter.next().unwrap(), "        } BF;");
+                }
+                ParseResult::Capture(r) => panic!("Should contain match"),
+            }
+        };
+    }
+
+    #[test]
+    fn parse_word() {
+        let teststring = String::from("        uint32_t WORD;");
+        let (state, parseresult) = parse(ParseState::Size, teststring, 0);
+        assert_eq!(state, ParseState::Name);
+        assert!(parseresult.is_some());
+        if let Some(x) = parseresult {
+            match x {
+                ParseResult::Match(r) => {
+                    let mut r_iter = r.iter();
+                    assert_eq!(r_iter.next().unwrap(), "        uint32_t WORD;");
+                }
+                ParseResult::Capture(r) => panic!("Should contain match"),
+            }
+        };
+    }
+
+    #[test]
+    fn parse_end_of_struct() {
+        let teststring = String::from("    } soc_info0;");
+        let (state, parseresult) = parse(ParseState::Name, teststring, 0);
+        assert_eq!(state, ParseState::BlockName);
+        assert!(parseresult.is_some());
+        if let Some(x) = parseresult {
+            match x {
+                ParseResult::Match(_) => panic!("Should contain capture"),
+                ParseResult::Capture(r) => {
+                    let mut r_iter = r.iter();
+                    assert_eq!(r_iter.next().unwrap(), "soc_info0");
+                }
+            }
+        };
+    }
+}
---
+++ tests/testfiles.rs
@@ -0,0 +1,181 @@
+#[cfg(test)]
+mod tests {
+    use bl808_sdk_header_tools::*;
+    #[test]
+    fn test_fragment() {
+        let header = r"
+struct glb_reg {
+    /* 0x0 : soc_info0 */
+    union {
+        struct {
+            uint32_t reserved_0_26 : 27; /* [26: 0],       rsvd,        0x0 */
+            uint32_t chip_rdy      : 1;  /* [   27],          r,        0x0 */
+        } BF;
+        uint32_t WORD;
+    } soc_info0;
+
+    /* 0xAE4 : gpio_cfg136 */
+    union {
+        struct {
+            uint32_t reg2_gpio_0_o  : 1; /* [    0],        r/w,        0x0 */
+        } BF;
+        uint32_t WORD;
+    } gpio_cfg136;
+";
+        let mut lines = header.lines();
+        let mut linenum = 0;
+
+        // first line is blank, should still be in nomatch state
+        let (state, parseresult) = parse(
+            ParseState::NoMatch,
+            lines.next().unwrap().to_string(),
+            linenum,
+        );
+        assert_eq!(state, ParseState::NoMatch);
+        assert!(parseresult.is_none());
+        linenum += 1;
+
+        // 2nd line is "struct glb_reg {"
+        let (state, parseresult) = parse(
+            ParseState::NoMatch,
+            lines.next().unwrap().to_string(),
+            linenum,
+        );
+        assert_eq!(state, ParseState::BlockName);
+        assert!(parseresult.is_some());
+        if let Some(x) = parseresult {
+            match x {
+                ParseResult::Match(_) => panic!("Should contain capture"),
+                ParseResult::Capture(r) => {
+                    let mut r_iter = r.iter();
+                    assert_eq!(r_iter.next().unwrap(), "glb_reg");
+                }
+            }
+        };
+        linenum += 1;
+
+        // 3nd line is "/* 0x0 : soc_info0 */"
+        let (state, parseresult) = parse(
+            ParseState::BlockName,
+            lines.next().unwrap().to_string(),
+            linenum,
+        );
+        assert_eq!(state, ParseState::BlockAddr);
+        assert!(parseresult.is_some());
+        if let Some(x) = parseresult {
+            match x {
+                ParseResult::Match(_) => panic!("Should contain capture"),
+                ParseResult::Capture(r) => {
+                    let mut r_iter = r.iter();
+                    assert_eq!(r_iter.next().unwrap(), "0x0");
+                    assert_eq!(r_iter.next().unwrap(), "soc_info0");
+                }
+            }
+        };
+        linenum += 1;
+
+        // 4th line is "union {"
+        let (state, parseresult) = parse(
+            ParseState::BlockAddr,
+            lines.next().unwrap().to_string(),
+            linenum,
+        );
+        assert_eq!(state, ParseState::UnionStr);
+        assert!(parseresult.is_some());
+        if let Some(x) = parseresult {
+            match x {
+                ParseResult::Match(r) => {
+                    let mut r_iter = r.iter();
+                    assert_eq!(r_iter.next().unwrap(), "    union {");
+                }
+                ParseResult::Capture(r) => panic!("Should contain match"),
+            }
+        };
+        linenum += 1;
+
+        // 5th line is "        struct {"
+        let (state, parseresult) = parse(
+            ParseState::UnionStr,
+            lines.next().unwrap().to_string(),
+            linenum,
+        );
+        assert_eq!(state, ParseState::StructStr);
+        assert!(parseresult.is_some());
+        if let Some(x) = parseresult {
+            match x {
+                ParseResult::Match(r) => {
+                    let mut r_iter = r.iter();
+                    assert_eq!(r_iter.next().unwrap(), "        struct {");
+                }
+                ParseResult::Capture(r) => panic!("Should contain match"),
+            }
+        };
+        linenum += 1;
+
+        // 5th line is "uint32_t reserved_0_26 : 27; /* [26: 0],       rsvd,        0x0 */"
+        let (state, parseresult) = parse(
+            ParseState::StructStr,
+            lines.next().unwrap().to_string(),
+            linenum,
+        );
+        assert_eq!(state, ParseState::StructStr);
+        assert!(parseresult.is_some());
+        if let Some(x) = parseresult {
+            match x {
+                ParseResult::Match(_) => panic!("Should contain capture"),
+                ParseResult::Capture(r) => {
+                    let mut r_iter = r.iter();
+                    assert_eq!(r_iter.next().unwrap(), "reserved_0_26");
+                    assert_eq!(r_iter.next().unwrap(), "27");
+                    assert_eq!(r_iter.next().unwrap(), "26: 0");
+                    assert_eq!(r_iter.next().unwrap(), "rsvd");
+                    assert_eq!(r_iter.next().unwrap(), "0x0");
+                }
+            }
+        };
+        linenum += 1;
+
+        // 6th line is "uint32_t chip_rdy      : 1;  /* [   27],          r,        0x0 */"
+        let (state, parseresult) = parse(
+            ParseState::StructStr,
+            lines.next().unwrap().to_string(),
+            linenum,
+        );
+        assert_eq!(state, ParseState::StructStr);
+        assert!(parseresult.is_some());
+        if let Some(x) = parseresult {
+            match x {
+                ParseResult::Match(_) => panic!("Should contain capture"),
+                ParseResult::Capture(r) => {
+                    let mut r_iter = r.iter();
+                    assert_eq!(r_iter.next().unwrap(), "chip_rdy");
+                    assert_eq!(r_iter.next().unwrap(), "1");
+                    assert_eq!(r_iter.next().unwrap(), "   27");
+                    assert_eq!(r_iter.next().unwrap(), "r");
+                    assert_eq!(r_iter.next().unwrap(), "0x0");
+                }
+            }
+        };
+        linenum += 1;
+
+        // 7th line is "} BF;"
+        let (state, parseresult) = parse(
+            ParseState::StructStr,
+            lines.next().unwrap().to_string(),
+            linenum,
+        );
+        assert_eq!(state, ParseState::Field);
+        assert!(parseresult.is_none());
+        linenum += 1;
+
+        // 8th line is "uint32_t WORD;"
+        let (state, parseresult) = parse(
+            ParseState::Field,
+            lines.next().unwrap().to_string(),
+            linenum,
+        );
+        assert_eq!(state, ParseState::Size);
+        assert!(parseresult.is_none());
+        linenum += 1;
+    }
+}
